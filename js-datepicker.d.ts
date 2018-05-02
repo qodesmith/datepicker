b84c28f266ef5565b6c9051e4f082e783f10709b
Puzzlefactory <alanbryant@acm.org>
2018-05-02 17:10:16 -0400

    js-datepicker.d.ts
    A definition file for Typescript so the library can be used in Typescript projects

---
+++ js-datepicker.d.ts
@@ -0,0 +1,11 @@
+/* Typescript definition file so can be imported and used in TS projects */
+
+export = js_datepicker;
+
+declare function js_datepicker(t: any, o: any): any;
+
+declare namespace js_datepicker {
+    const prototype: {
+    };
+
+}

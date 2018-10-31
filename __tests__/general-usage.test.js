02ede5753b006154a2cbd27aa77afd83e43040f7
Aaron Cordova <theqodesmith@gmail.com>
2018-10-30 21:46:06 -0400

    Progress on general usage test.

--- __tests__/general-usage.test.js
+++ __tests__/general-usage.test.js
@@ -4,13 +4,13 @@ describe('General Usage' ,() => {
   let picker = undefined
 
   beforeEach(() => {
-    document.body.innerHTML = '<input type="text" id="input1" />'
-    picker = datepicker('#input1')
+    document.body.innerHTML = '<input type="text" />'
+    picker = datepicker('input')
   })
 
-  afterEach(picker.remove)
+  afterEach(() => picker.remove())
 
-  it.only('should show / hide the calendar when the input is focused / blurred', () => {
+  it('should show / hide the calendar when the input is focused / blurred', () => {
     const dp = document.querySelector('.qs-datepicker')
 
     const allPickers = document.querySelectorAll('.qs-datepicker')
@@ -19,26 +19,54 @@ describe('General Usage' ,() => {
     expect(!!dp).toBe(true)
     expect(dp.className).toBe('qs-datepicker qs-hidden')
 
-    document.querySelector('#input1').dispatchEvent(new FocusEvent('focusin', { bubbles: true }))
+    document.querySelector('input').dispatchEvent(new FocusEvent('focusin', { bubbles: true }))
     expect(dp.className).toBe('qs-datepicker')
 
     document.body.click()
     expect(dp.className).toBe('qs-datepicker qs-hidden')
   })
 
-  it('should close when selecting a date on the calendar', () => {
+  it('should hide the calendar when selecting a date on the calendar', () => {
     const dp = document.querySelector('.qs-datepicker')
+    const dayOne = document.querySelector('.qs-square.qs-num:not(.qs-empty)')
 
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
 
+    document.querySelector('input').dispatchEvent(new FocusEvent('focusin', { bubbles: true }))
+    expect(dp.className).toBe('qs-datepicker')
+
+    dayOne.click()
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
   })
 
-  it('should highlight the selected day on the calendar', () => {})
+  it('should highlight the selected day on the calendar', () => {
+    const dp = document.querySelector('.qs-datepicker')
+    const dayOne = document.querySelector('.qs-square.qs-num:not(.qs-empty)')
 
-  it('should change the month when the arrows are clicked', () => {})
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
 
-  it('should do nothing when the weekday names are clicked', () => {})
+    expect(dayOne.classList.contains('qs-square')).toBe(true)
+    expect(dayOne.classList.contains('qs-num')).toBe(true)
+    expect(dayOne.classList.contains('qs-active')).toBe(false)
+    expect(dayOne.textContent).toBe('1')
 
-  it('should show todays date in bold', () => {})
+    document.querySelector('input').dispatchEvent(new FocusEvent('focusin', { bubbles: true }))
+    expect(dp.className).toBe('qs-datepicker')
 
-  it('should have no disabled dates', () => {})
+    dayOne.click()
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
+
+    document.querySelector('input').dispatchEvent(new FocusEvent('focusin', { bubbles: true }))
+    expect(dayOne.classList.contains('qs-square')).toBe(true)
+    expect(dayOne.classList.contains('qs-num')).toBe(true)
+    expect(dayOne.classList.contains('qs-active')).toBe(true)
+  })
+
+  // it('should change the month when the arrows are clicked', () => {})
+
+  // it('should do nothing when the weekday names are clicked', () => {})
+
+  // it('should show todays date in bold', () => {})
+
+  // it('should have no disabled dates', () => {})
 })

f095ae708aee007e045bc27d7aa2c5e28063262c
Aaron Cordova <theqodesmith@gmail.com>
2018-10-30 21:21:06 -0400

    Initialization test complete.

--- __tests__/initialization.test.js
+++ __tests__/initialization.test.js
@@ -1,26 +1,52 @@
 const datepicker = require('../datepicker')
 
 describe('Initialization' ,() => {
-  beforeEach(() => document.body.innerHTML = '<input type="text" id="input1" />')
+  let picker = undefined
+
+  beforeEach(() => document.body.innerHTML = '<input type="text" />')
+
+  afterEach(() => picker.remove())
 
   it('should take in a string for the 1st argument as a selector', () => {
-    const picker = datepicker('#input1')
-    const dp = document.querySelector('.qs-datepicker')
+    picker = datepicker('input')
+    const dp = document.querySelector('.qs-datepicker.qs-hidden')
 
     expect(!!dp).toBe(true)
-    picker.remove()
   })
 
   it('should take in a DOM node for the 1st argument as a selector', () => {
-    const input = document.querySelector('#input1')
-    const picker = datepicker(input)
-    const dp = document.querySelector('.qs-datepicker')
+    picker = datepicker(document.querySelector('input'))
+    const dp = document.querySelector('.qs-datepicker.qs-hidden')
 
     expect(!!dp).toBe(true)
-    picker.remove()
   })
 
-  it('should show / hide the calendar when "attached" to an <input> element', () => {})
+  it('should show / hide the calendar when "attached" to an <input> element', () => {
+    picker = datepicker('input')
+    const dp = document.querySelector('.qs-datepicker')
 
-  it('should show / hide the calendar when "attached" to a non-input element', () => {})
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
+
+    document.querySelector('input').dispatchEvent(new FocusEvent('focusin', { bubbles: true }))
+    expect(dp.className).toBe('qs-datepicker')
+
+    document.body.click()
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
+  })
+
+  it('should show / hide the calendar when "attached" to a non-input element', () => {
+    picker = datepicker(document.body)
+    const dp = document.querySelector('.qs-datepicker')
+
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
+
+    document.querySelector('input').dispatchEvent(new FocusEvent('focusin', { bubbles: true }))
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
+
+    document.body.click()
+    expect(dp.className).toBe('qs-datepicker')
+
+    document.body.click()
+    expect(dp.className).toBe('qs-datepicker qs-hidden')
+  })
 })

82dcdda4bc5339fbb55e8bb880970d5e2e20d85a
Aaron Cordova <theqodesmith@gmail.com>
2020-02-18 07:24:49 -0500

    We don't need the less mock with Cypress.

--- __mocks__/lessMock.js
+++
@@ -1,7 +0,0 @@
-/*
-  https://jestjs.io/docs/en/webpack#handling-static-assets
-  This mocks the require statement for the less file at
-  the top of datepicker.js so that the tests don't fail.
-*/
-
-module.exports = {}

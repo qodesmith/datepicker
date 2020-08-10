a6260c1a63d64905434474494ea8145efe114f7f
Qodesmith <theqodesmith@gmail.com>
2020-08-09 21:58:06 -0400

    Finished tests for `formatter`.

--- cypress/integration/options.js
+++ cypress/integration/options.js
@@ -1,4 +1,12 @@
-// TODO - implement these tests.
+import selectors from '../selectors'
+
+const {
+  singleDatepickerInput,
+  single,
+  range,
+  common,
+  singleDatepickerInputParent,
+} = selectors
 
 describe('User options', function() {
   beforeEach(function() {
@@ -13,6 +21,69 @@ describe('User options', function() {
     cy.window().then(global => cy.wrap(global.datepicker).as('datepicker'))
   })
 
-  describe('Single instance', function() {})
-  describe('Daterange pair', function() {})
+  describe('Customizations', function() {
+    describe('formatter', function() {
+      it('should customize the input value when a date is selected', function() {
+        const expectedValue = 'datepicker rulez'
+        const options = {
+          formatter: (input, date, instance) => {
+            input.value = expectedValue
+          }
+        }
+        this.datepicker(singleDatepickerInput, options)
+
+        cy.get(singleDatepickerInput).should('have.value', '').click()
+        cy.get(`${single.calendarContainer} [data-direction="0"]`).first().click()
+        cy.get(singleDatepickerInput).should('have.value', expectedValue)
+      })
+
+      it('should be called with the correct arguments', function() {
+        let picker
+        const today = new Date()
+        const selectedDate = new Date(today.getFullYear(), today.getMonth(), 1)
+        const options = {
+          formatter: (input, date, instance) => {
+            expect(input, '1st arg to `formatter` should be the input').to.eq(instance.el)
+
+            /*
+              We can't use `instanceof Date` because `Date` is a different constructor
+              than the one on the window object that Cypress uses. Essentially,
+              we're dealing with 2 different window object. So it's easier to just do
+              the whole toString thingy.
+            */
+            expect(({}).toString.call(date), '2nd arg to `formatter` should be the date selected').to.eq('[object Date]')
+            expect(+instance.dateSelected, 'the date should === instance.dateSelected').to.eq(+date)
+            expect(+selectedDate, 'the selected date should have the correct value').to.eq(+date)
+            expect(instance, '3rd arg to `formatter` should be the instance').to.eq(picker)
+          }
+        }
+
+        picker = this.datepicker(singleDatepickerInput, options)
+        cy.get(singleDatepickerInput).click()
+        cy.get(`${single.calendarContainer} [data-direction="0"]`).first().click()
+      })
+
+      it(`should not be called if the picker doesn't have an associated input`, function() {
+        const options = { formatter: () => {} }
+        const spy = cy.spy(options, 'formatter')
+        this.datepicker(singleDatepickerInputParent, options)
+
+        cy.get(singleDatepickerInputParent).click({ force: true })
+        cy.get(`${common.squaresContainer} [data-direction="0"]`).first().click().then(() => {
+          expect(spy).not.to.be.called
+        })
+      })
+    })
+
+    describe('position', function() {
+      it('should position the calendar relative to the input - default (bottom left)', function() {
+        this.datepicker(singleDatepickerInput)
+      })
+
+      it('should position the calendar relative to the input - bottom left', function() {})
+      it('should position the calendar relative to the input - bottom right', function() {})
+      it('should position the calendar relative to the input - top left', function() {})
+      it('should position the calendar relative to the input - top right', function() {})
+    })
+  })
 })

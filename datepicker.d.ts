80bdc3f4f440e5f5215219d30e361c5d60f55287
Qodesmith <theqodesmith@gmail.com>
2022-11-30 11:17:49 -0500

    Typing complete!

--- datepicker.d.ts
+++ datepicker.d.ts
@@ -1,5 +1,5 @@
-declare module 'js-datepicer' {
-  type DatepickerOptions = {
+declare module 'js-datepicker' {
+  export type DatepickerOptions = {
     /**
      * Callback function after a date has been selected. The 2nd argument is the selected date when a date is being selected and `undefined` when a date is being unselected. You unselect a date by clicking it again.
      */
@@ -170,7 +170,7 @@ declare module 'js-datepicer' {
      *
      * Default - false
      */
-    noWeekends: boolean
+    noWeekends?: boolean
 
     /**
      * Sometimes you need more control over which dates to disable. The `disabledDates` option is limited to an explicit array of dates and the `noWeekends` option is limited to Saturdays & Sundays. Provide a function that takes a JavaScript date as it's only argument and returns `true` if the date should be disabled. When the calendar builds, each date will be run through this function to determine whether or not it should be disabled.
@@ -197,10 +197,138 @@ declare module 'js-datepicer' {
     disableYearOverlay?: boolean
   }
 
-  type DatepickerInstance = {}
+  export type DaterangePickerOptions = DatepickerOptions & {
+    /**
+     * Now we're getting fancy! If you want to link two instances together to help form a daterange picker, this is your option. Only two picker instances can share an `id`. The datepicker instance declared first will be considered the "start" picker in the range. There's a fancy `getRange` method for you to use as well.
+     */
+    id: any
+  }
 
-  export default function datepicker(
-    selector: string | HTMLElement,
-    DatepickerOptions
+  export type DatepickerInstance = {
+    /**
+     * TODO - move this into the initialize options.
+     * Manually set this property to `true` to fully disable the calendar.
+     */
+    disabled: boolean
+
+    /**
+     * Performs cleanup. This will remove the current instance from the DOM, leaving all others in tact. If this is the only instance left, it will also remove the event listeners that Datepicker previously set up.
+     */
+    remove(): void
+
+    /**
+     * Programmatically navigates the calendar to the date you provide. This doesn't select a date, it's literally just for navigation. You can optionally trigger the `onMonthChange` callback with the 2nd argument.
+     */
+    navigate(date: Date, triggerOnMonthChange?: boolean): void
+
+    /**
+     * Allows you to programmatically select or unselect a date on the calendar. To select a date on the calendar, pass in a JS date object for the 1st argument. If you set a date on a month other than what's currently displaying and you want the calendar to automatically change to it, pass in `true` as the 2nd argument.
+     * Want to unselect a date? Simply run the function with no arguments.
+     *
+     * NOTE: This will not trigger the `onSelect` callback.
+     */
+    setDate(date: Date, changeCalendar?: boolean): void
+    setDate(): void
+
+    /**
+     * Allows you to programmatically set the minimum selectable date or unset it. If this instance is part of a daterange instance (see the `id` option) then the other instance will be changed as well. To unset a minimum date, simply run the function with no arguments.
+     */
+    setMin(date: Date): void
+    setMin(): void
+
+    /**
+     * Allows you to programmatically set the maximum selectable date or unset it. If this instance is part of a daterange instance (see the `id` option) then the other instance will be changed as well. To unset a maximum date, simply run the function with no arguments.
+     */
+    setMax(date: Date): void
+    setMax(): void
+
+    /**
+     * Allows you to programmatically show the calendar. Using this method will trigger the `onShow` callback if your instance has one.
+     *
+     * NOTE: Want to show / hide the calendar programmatically with a button or by clicking some element? Make sure to use `stopPropagation` in your event callback! If you don't, any click event in the DOM will bubble up to Datepicker's internal oneHandler event listener, triggering logic to close the calendar since it "sees" the click event outside the calendar.
+     */
+    show(): void
+
+    /**
+     * Allows you to programmatically hide the calendar. If the `alwaysShow` property was set on the instance then this method will have no effect. Using this method will trigger the `onHide` callback if your instance has one.
+     *
+     * NOTE: Want to show / hide the calendar programmatically with a button or by clicking some element? Make sure to use `stopPropagation` in your event callback! If you don't, any click event in the DOM will bubble up to Datepicker's internal oneHandler event listener, triggering logic to close the calendar since it "sees" the click event outside the calendar.
+     */
+    hide(): void
+
+    /**
+     * Call this method on the picker to programmatically toggle the overlay. This will only work if the calendar is showing!
+     */
+    toggleOverlay(): void
+
+    /**
+     * The calendar element.
+     */
+    calendar: HTMLElement
+
+    /**
+     * The container element that houses the calendar. Use it to size the calendar or programmatically check if the calendar is showing.
+     */
+    calendarContainer: HTMLElement
+
+    /**
+     * A 0-index number representing the current month. For example, 0 represents January.
+     */
+    currentMonth: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11
+
+    /**
+     * Calendar month in plain english. E.x. January
+     */
+    currentMonthName: string
+
+    /**
+     * The current year. E.x. 2099
+     */
+    currentYear: number
+
+    /**
+     * The value of the selected date. This will be `undefined` if no date has been selected yet.
+     */
+    dateSelected: Date | undefined
+
+    /**
+     * The element datepicker is relatively positioned against (unless centered).
+     */
+    el: Element
+
+    /**
+     * The minimum selectable date.
+     */
+    minDate: Date | undefined
+
+    /**
+     * The maximum selectable date.
+     */
+    maxDate: Date | undefined
+  }
+
+  export type DaterangePickerInstance = DatepickerInstance & {
+    /**
+     * This method is only available on daterange pickers. It will return an object with `start` and `end` properties whose values are JavaScript date objects representing what the user selected on both calendars.
+     */
+    getRange(): {start: Date | undefined; end: Date | undefined}
+
+    /**
+     * If two datepickers have the same `id` option then this property will be available and refer to the other instance.
+     */
+    sibling: DaterangePickerInstance
+  }
+
+  export type Selector = string | HTMLElement
+
+  function datepicker(
+    selector: Selector,
+    options?: DatepickerOptions
   ): DatepickerInstance
+  function datepicker(
+    selector: Selector,
+    options: DaterangePickerOptions
+  ): DaterangePickerInstance
+
+  export default datepicker
 }

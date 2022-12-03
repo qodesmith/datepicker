3543c0cd93f32ecb8433d1698a0275a51582a454
Qodesmith <theqodesmith@gmail.com>
2022-12-03 12:24:20 -0500

    Better usage of dangerouslySetInnerHTML.

--- sandbox/App.tsx
+++ sandbox/App.tsx
@@ -3,12 +3,12 @@ import datepicker from '../src/datepicker'
 
 function App() {
   useEffect(() => {
-    const picker = datepicker('test', {})
+    const picker = datepicker('.test', {})
   }, [])
   return (
     <>
       <div>Hello world!</div>
-      <div dangerouslySetInnerHTML={{__html: '<div class="test"></div>'}} />
+      <div className="test" dangerouslySetInnerHTML={{__html: ''}} />
     </>
   )
 }

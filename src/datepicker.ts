b616ccc2235a7beeeacb683d211077698e8f9e85
Qodesmith <theqodesmith@gmail.com>
2022-12-04 17:19:26 -0500

    Updated getSelectorData function.

--- src/datepicker.ts
+++ src/datepicker.ts
@@ -1,16 +1,24 @@
 import type {
   DatepickerInstance,
   DatepickerOptions,
+  DatepickersMapItem,
   DaterangePickerInstance,
   DaterangePickerOptions,
   Selector,
 } from './types'
 
-import getSelectorEl from './getSelectorEl'
+import getSelectorData from './getSelectorData'
+
+const datepickersMap = new Map<
+  DatepickerInstance | DaterangePickerInstance,
+  DatepickersMapItem
+>()
+
+datepickersMap.set
 
 export default function datepicker(
   selector: Selector,
-  options: DatepickerOptions | DaterangePickerOptions
-): DatepickerInstance | DaterangePickerInstance {
-  const el = getSelectorEl(selector)
+  options?: DatepickerOptions | DaterangePickerOptions
+) /*DatepickerInstance | DaterangePickerInstance*/ {
+  const {el, shadowDom, customElement} = getSelectorData(selector)
 }
---
+++ src/getSelectorData.ts
@@ -0,0 +1,69 @@
+import getType from './getType'
+import throwError from './throwError'
+import {Selector} from './types'
+
+type GetElReturnType = {
+  el: HTMLElement
+  shadowDom: null | ShadowRoot
+  customElement: null | Element
+}
+
+export default function getSelectorData(selector: Selector): GetElReturnType {
+  let element: HTMLElement | null = null
+  const type = getType(selector)
+
+  // Find the element via the provided string selector.
+  if (typeof selector === 'string') {
+    /**
+     * In the case that the selector is an id beginning with a number
+     * (e.x. #123), querySelector will fail. That's why we need to check and
+     * conditionally use `getElementById`. Also, datepicker doesn't support
+     * string selectors when using a shadow DOM, hence why we use `document`.
+     */
+    const el =
+      selector[0] === '#'
+        ? document.getElementById(selector.slice(1))
+        : document.querySelector<HTMLElement>(selector)
+
+    if (el === null) {
+      throwError(`No element found for selector "${selector}".`)
+    }
+
+    element = el
+  }
+
+  // Here, the user has already passed in an HTML element.
+  if (type.endsWith('Element')) {
+    element = selector as HTMLElement
+  }
+
+  if (type === 'ShadowRoot') {
+    throwError('Using a shadow DOM as your selector is not supported.')
+  }
+
+  if (!element) {
+    throwError(`The selector provided is not a valid HTML element: ${type}`)
+  }
+
+  const rootNode = element.getRootNode()
+  const rootNodeType = getType(rootNode)
+
+  /**
+   * There are only 2 possible root nodes supported:
+   *   * document
+   *   * a shadow DOM
+   */
+  if (rootNodeType === 'HTMLDocument') {
+    return {el: element, shadowDom: null, customElement: null}
+  }
+
+  if (rootNodeType === 'ShadowRoot') {
+    return {
+      el: element,
+      shadowDom: rootNode as ShadowRoot,
+      customElement: (rootNode as ShadowRoot).host,
+    }
+  }
+
+  throwError(`Invalid root node found for selector: ${rootNodeType}`)
+}

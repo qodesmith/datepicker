428e7c27716d622650e0628b143bb8f63dfa480a
Qodesmith <theqodesmith@gmail.com>
2022-12-04 17:37:20 -0500

    Added comments for intellisense.

--- src/checkForExistingPicker.ts
+++ src/checkForExistingPicker.ts
@@ -1,6 +1,9 @@
 import {datepickersMap} from './constants'
 import throwError from './throwError'
 
+/**
+ * Checks if a picker already exists on a given element. If so, it throws.
+ */
 export default function checkForExistingPicker(el: HTMLElement): void {
   if (datepickersMap.has(el)) {
     throwError('A datepicker already exists on that element.')

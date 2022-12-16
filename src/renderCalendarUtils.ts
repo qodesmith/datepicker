edea7e74954293f07e7b897cf30a0813a45415af
Qodesmith <theqodesmith@gmail.com>
2022-12-16 13:11:34 -0500

    Updating class names for calendar days.

--- src/renderCalendarUtils.ts
+++ src/renderCalendarUtils.ts
@@ -1,11 +1,14 @@
+import {isDateWithinRange, stripTime} from './generalUtils'
 import getDaysInMonth from './getDaysInMonth'
 import {InternalPickerData} from './types'
 
 export function renderCalendar(picker: InternalPickerData): void {
   const currentYear = picker.currentDate.getFullYear()
   const currentMonthNum = picker.currentDate.getMonth()
-  const currentMonth = picker.months[currentMonthNum]
+  const currentMonthName = picker.months[currentMonthNum]
   const daysInMonth = getDaysInMonth(picker.currentDate)
+  const {selectedDate, minDate, maxDate, disabledDates} = picker
+  const selectedDateNum = selectedDate ? +stripTime(selectedDate) : null
 
   /**
    * Iterate through the calendar days and hide any days that are beyond the
@@ -13,17 +16,37 @@ export function renderCalendar(picker: InternalPickerData): void {
    */
   picker.pickerElements.calendarDaysArray.forEach((el, i) => {
     const num = i + 1
+    const dateForComparison = new Date(currentYear, currentMonthNum, num)
+    const dateNumForComparison = +dateForComparison
+    const dateInRange = isDateWithinRange({
+      date: dateForComparison,
+      minDate,
+      maxDate,
+    })
 
+    // Apply / remove displayed date styles.
     if (num <= daysInMonth) {
       el.classList.remove('dp-dn')
     } else {
       el.classList.add('dp-dn')
     }
+
+    // Apply / remove selected date styles.
+    if (selectedDateNum === dateNumForComparison && dateInRange) {
+      el.classList.add('dp-selected-date')
+    } else {
+      el.classList.remove('dp-selected-date')
+    }
+
+    // Apple / remove disabled date styles.
+    if (!dateInRange || disabledDates.has(dateNumForComparison)) {
+      el.classList.add('dp-disabled-date')
+    } else {
+      el.classList.remove('dp-disabled-date')
+    }
   })
 
-  // Adjust the month and year in the calendar controls.
-  picker.pickerElements.controls.monthName.textContent = currentMonth
+  // Adjust the month name and year in the calendar controls.
+  picker.pickerElements.controls.monthName.textContent = currentMonthName
   picker.pickerElements.controls.year.textContent = `${currentYear}`
-
-  //
 }

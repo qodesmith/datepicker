57afd4fccddd15a22870311be7ae027e88ce4b14
Qodesmith <theqodesmith@gmail.com>
2022-12-20 21:21:30 -0500

    Adjusted layout of test app.

--- sandbox/App.tsx
+++ sandbox/App.tsx
@@ -61,100 +61,108 @@ function App() {
             '<input type="text" class="old-dp-input" placeholder="old-dp-input" />',
         }}
       />
-      <div
-        style={{
-          border: '1px solid',
-          padding: '.5em',
-          margin: '.5em',
-          display: 'table',
-        }}>
-        <h2>Data</h2>
-        <div>
-          <code style={{fontSize: '.75em', color: 'gray'}}>
-            getComputedStyle(picker.calendarContainer).fontSize
-          </code>
-        </div>
-        <div>
-          <code>.dp-calendar-container</code> font size:{' '}
-          <code style={{color: 'lime'}}>{fontSize ?? 'null'}</code>
-        </div>
-        <br />
-        <div>
-          <code style={{fontSize: '.75em', color: 'gray'}}>
-            getComputedStyle(picker.calendarContainer).width
-          </code>
-        </div>
-        <div>
-          <code>.dp-calendar-container</code> width:{' '}
-          <code style={{color: 'lime'}}>{width ?? 'null'}</code>
-        </div>
-        <hr />
-        <div>
-          Change font size (em) of the calendar{' '}
-          <strong>
-            <em>container parent element</em>
-          </strong>
-          :
-          <br />
-          <input
-            type="range"
-            step=".01"
-            min=".3"
-            max="4"
-            value={parentElementFontSize}
-            onChange={e => setParentElementFontSize(+e.target.value)}
-          />{' '}
-          <code style={{color: 'lime'}}>
-            {parentElementFontSize.toFixed(2)}em,{' '}
-            {parentElementFontSizeComputed
-              ? Number(parentElementFontSizeComputed.replace('px', '')).toFixed(
-                  2
-                ) + 'px'
-              : 'null'}
-          </code>
-          <div style={{display: 'flex', gap: '.5em'}}>
-            <button onClick={() => setParentElementFontSize(0.5)}>.5em</button>
-            <button onClick={() => setParentElementFontSize(1)}>1em</button>
-            <button onClick={() => setParentElementFontSize(2)}>2em</button>
-            <button onClick={() => setParentElementFontSize(3)}>3em</button>
-            <button onClick={() => setParentElementFontSize(4)}>4em</button>
+      <div style={{display: 'flex', gap: '2em'}}>
+        {/* DATA */}
+        <section
+          style={{
+            border: '1px solid',
+            padding: '.5em',
+            margin: '.5em',
+          }}>
+          <h2>Data</h2>
+          <div>
+            <code style={{fontSize: '.75em', color: 'gray'}}>
+              getComputedStyle(picker.calendarContainer).fontSize
+            </code>
           </div>
-        </div>
-        <hr />
-        <div>
-          Change the <code>--dp-size</code> value on{' '}
-          <code>
+          <div>
+            <code>.dp-calendar-container</code> font size:{' '}
+            <code style={{color: 'lime'}}>{fontSize ?? 'null'}</code>
+          </div>
+          <br />
+          <div>
+            <code style={{fontSize: '.75em', color: 'gray'}}>
+              getComputedStyle(picker.calendarContainer).width
+            </code>
+          </div>
+          <div>
+            <code>.dp-calendar-container</code> width:{' '}
+            <code style={{color: 'lime'}}>{width ?? 'null'}</code>
+          </div>
+          <hr />
+          <div>
+            Change font size (em) of the calendar{' '}
             <strong>
-              <em>.dp-calendar-container</em>
+              <em>container parent element</em>
             </strong>
-          </code>
-          :
-          <br />
-          <input
-            type="range"
-            step=".01"
-            min=".1"
-            max="3"
-            value={size}
-            onChange={e => setSize(+e.target.value)}
-          />
-          <code style={{color: 'lime'}}>{size.toFixed(3) + 'em'}</code>
-          <div style={{display: 'flex', gap: '.5em'}}>
-            <button onClick={() => setSize(0.5)}>.5em</button>
-            <button onClick={() => setSize(0.75)}>.75em</button>
-            <button onClick={() => setSize(1)}>1em</button>
-            <button onClick={() => setSize(2)}>2em</button>
-            <button onClick={() => setSize(3)}>3em</button>
+            :
+            <br />
+            <input
+              type="range"
+              step=".01"
+              min=".3"
+              max="4"
+              value={parentElementFontSize}
+              onChange={e => setParentElementFontSize(+e.target.value)}
+            />{' '}
+            <code style={{color: 'lime'}}>
+              {parentElementFontSize.toFixed(2)}em,{' '}
+              {parentElementFontSizeComputed
+                ? Number(
+                    parentElementFontSizeComputed.replace('px', '')
+                  ).toFixed(2) + 'px'
+                : 'null'}
+            </code>
+            <div style={{display: 'flex', gap: '.5em'}}>
+              <button onClick={() => setParentElementFontSize(0.5)}>
+                .5em
+              </button>
+              <button onClick={() => setParentElementFontSize(1)}>1em</button>
+              <button onClick={() => setParentElementFontSize(2)}>2em</button>
+              <button onClick={() => setParentElementFontSize(3)}>3em</button>
+              <button onClick={() => setParentElementFontSize(4)}>4em</button>
+            </div>
           </div>
-        </div>
+          <hr />
+          <div>
+            Change the <code>--dp-size</code> value on{' '}
+            <code>
+              <strong>
+                <em>.dp-calendar-container</em>
+              </strong>
+            </code>
+            :
+            <br />
+            <input
+              type="range"
+              step=".01"
+              min=".1"
+              max="3"
+              value={size}
+              onChange={e => setSize(+e.target.value)}
+            />
+            <code style={{color: 'lime'}}>{size.toFixed(3) + 'em'}</code>
+            <div style={{display: 'flex', gap: '.5em'}}>
+              <button onClick={() => setSize(0.5)}>.5em</button>
+              <button onClick={() => setSize(0.75)}>.75em</button>
+              <button onClick={() => setSize(1)}>1em</button>
+              <button onClick={() => setSize(2)}>2em</button>
+              <button onClick={() => setSize(3)}>3em</button>
+            </div>
+          </div>
+        </section>
+
+        {/* DATEPICKER */}
+        <section>
+          <div>⬇⬇⬇ This is a div that will container a picker.</div>
+          <div
+            className="dp-test"
+            dangerouslySetInnerHTML={{
+              __html: '',
+            }}
+          />
+        </section>
       </div>
-      <div>⬇⬇⬇ This is a div that will container a picker.</div>
-      <div
-        className="dp-test"
-        dangerouslySetInnerHTML={{
-          __html: '',
-        }}
-      />
       <div
         className="shadow-dom-test"
         dangerouslySetInnerHTML={{

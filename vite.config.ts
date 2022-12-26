0e684b70c0f512710e8920b72a1f5e0e5d83748f
Qodesmith <theqodesmith@gmail.com>
2022-12-25 20:37:20 -0500

    Updated Vite config for bundling.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/datepicker.ts'),
+      name: 'datepicker',
+      fileName: 'dp',
+    },
   },
 })

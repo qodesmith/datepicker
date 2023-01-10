1e5631113eb951e772a07b2b7fc644bbdf4044c8
Qodesmith <theqodesmith@gmail.com>
2023-01-09 20:50:03 -0500

    NewApp => App

--- sandbox/main.tsx
+++ sandbox/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RecoilRoot} from 'recoil'
-import NewApp from './NewApp'
+import App from './App'
 import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-      <NewApp />
+      <App />
     </RecoilRoot>
   </React.StrictMode>
 )

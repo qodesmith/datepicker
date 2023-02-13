195beb6cd38280f06b0b9efe619c22f152a2b3f0
Qodesmith <theqodesmith@gmail.com>
2023-02-13 17:24:38 -0500

    Ignoring some test files.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import {resolve} from 'path'
 import './commonEnv.js'
 
 export default defineConfig(({mode}) => {
+  console.log('MODE:', mode)
   const isTest = mode === 'test'
   const envPortKey = isTest ? 'TEST_DEV_PORT' : 'DEV_PORT'
   const plugins = isTest ? [] : [react()]
@@ -23,5 +24,6 @@ export default defineConfig(({mode}) => {
         fileName: 'dp',
       },
     },
+    clearScreen: false,
   }
 })

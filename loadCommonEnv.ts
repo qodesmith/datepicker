80daff4e72efd5383eb348be673d3e2a732f412d
Qodesmith <theqodesmith@gmail.com>
2023-02-13 08:42:10 -0500

    Vite dev server conditionally running test-app instead.

--- loadCommonEnv.ts
+++ loadCommonEnv.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
 
 const envConfig = dotenv.config({path: './.common.env'})
+
+// Allows previously declared variables to be used as values.
 dotenvExpand.expand(envConfig)
 
 export default envConfig
--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,25 @@ import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
 import './loadCommonEnv'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: +process.env.DEV_PORT,
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/datepicker.ts'),
-      name: 'datepicker',
-      fileName: 'dp',
+export default defineConfig(({mode}) => {
+  const isTest = mode === 'test'
+  const envPortKey = isTest ? 'TEST_DEV_PORT' : 'DEV_PORT'
+  const plugins = isTest ? [] : [react()]
+  const root = isTest ? './tests/test-app' : './'
+
+  return {
+    plugins,
+    root,
+    server: {
+      host: true,
+      port: +process.env[envPortKey],
     },
-  },
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/datepicker.ts'),
+        name: 'datepicker',
+        fileName: 'dp',
+      },
+    },
+  }
 })

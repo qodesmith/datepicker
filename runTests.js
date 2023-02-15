6df966292cb51e67ff754f50cf412f1a3c1e4fc8
Qodesmith <theqodesmith@gmail.com>
2023-02-15 15:10:27 -0500

    Throwing error for faulty CLI args in runTests.js

--- runTests.js
+++ runTests.js
@@ -18,6 +18,9 @@ const config = {
 }
 
 const {type} = parseArgs(config).values
+if (type !== 'open' && type !== 'run') {
+  throw new Error('There are only 2 options for --type: `open` and `run`.')
+}
 
 execSync(
   `./node_modules/.bin/start-server-and-test dev-test ${TEST_DEV_LOCALHOST} cy:${type}`,

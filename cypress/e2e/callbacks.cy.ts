2048ef1136dab80c7b4b2506f6da1fc636c5f913
Qodesmith <theqodesmith@gmail.com>
2023-02-16 15:14:43 -0500

    Added tests for onMonthChange.

--- cypress/e2e/callbacks.cy.ts
+++ cypress/e2e/callbacks.cy.ts
@@ -1,5 +1,5 @@
 import {Datepicker, DatepickerOptions} from '../../src/types'
-import {days, testElements} from '../selectors'
+import {days, testElements, controls} from '../selectors'
 
 describe('Callbacks', () => {
   let datepicker: Datepicker
@@ -149,6 +149,110 @@ describe('Callbacks', () => {
           trigger: 'setMax',
           triggerType: 'imperative',
         })
+    })
+  })
+
+  describe('onMonthChange', () => {
+    const startDate = new Date(2023, 1)
+    const options: DatepickerOptions = {
+      onMonthChange() {},
+      alwaysShow: true,
+      startDate,
+    }
+
+    beforeEach(() => {
+      cy.spy(options, 'onMonthChange').as('onMonthChange')
+    })
+
+    it('should be called when clicking the arrow buttons', () => {
+      const picker = datepicker(testElements.singleInput, options)
+
+      expect(options.onMonthChange).not.to.be.called
+      const newDate = new Date(startDate)
+      newDate.setMonth(newDate.getMonth() - 1)
+
+      cy.get(controls.leftArrow).click()
+      cy.get('@onMonthChange')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', {
+          instance: picker,
+          trigger: 'click',
+          triggerType: 'user',
+          prevDate: startDate,
+          newDate,
+        })
+
+      cy.get(controls.rightArrow).click()
+      cy.get('@onMonthChange')
+        .should('have.been.calledTwice')
+        .should('have.been.calledWith', {
+          instance: picker,
+          trigger: 'click',
+          triggerType: 'user',
+          prevDate: newDate,
+          newDate: startDate,
+        })
+    })
+
+    it('should be called after running the `navigate` method', () => {
+      const picker = datepicker(testElements.singleInput, options)
+
+      expect(options.onMonthChange).not.to.be.called
+      const newDate = new Date(startDate)
+      newDate.setFullYear(newDate.getFullYear() + 1)
+
+      picker.navigate({date: newDate})
+      cy.get('@onMonthChange')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', {
+          instance: picker,
+          trigger: 'navigate',
+          triggerType: 'imperative',
+          prevDate: startDate,
+          newDate,
+        })
+    })
+
+    it('should be called after running `selectDate` with `changeCalendar: true`', () => {
+      const picker = datepicker(testElements.singleInput, options)
+
+      expect(options.onMonthChange).not.to.be.called
+      const newDate = new Date(startDate)
+      newDate.setFullYear(newDate.getFullYear() + 1)
+
+      picker.selectDate({date: newDate, changeCalendar: true})
+      cy.get('@onMonthChange')
+        .should('have.been.calledOnce')
+        .should('have.been.calledWith', {
+          instance: picker,
+          trigger: 'selectDate',
+          triggerType: 'imperative',
+          prevDate: startDate,
+          newDate,
+        })
+    })
+
+    it('should not be called after running `selectDate` with `changeCalendar: false`', () => {
+      const picker = datepicker(testElements.singleInput, options)
+
+      expect(options.onMonthChange).not.to.be.called
+      const newDate = new Date(startDate)
+      newDate.setFullYear(newDate.getFullYear() + 1)
+
+      picker.selectDate({date: newDate, changeCalendar: false})
+      cy.get('@onMonthChange').should('not.have.been.called')
+    })
+
+    it('should not be called after running `selectDate` with `changeCalendar: true` and no date', () => {
+      const picker = datepicker(testElements.singleInput, {
+        ...options,
+        selectedDate: startDate,
+      })
+
+      expect(options.onMonthChange).not.to.be.called
+
+      picker.selectDate({changeCalendar: true})
+      cy.get('@onMonthChange').should('not.have.been.called')
     })
   })
 })

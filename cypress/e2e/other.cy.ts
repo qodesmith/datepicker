da0788bcc0ef1cc4bbadd7b1179ac37a2c21c341
Qodesmith <theqodesmith@gmail.com>
2023-02-19 13:55:35 -0500

    Added test for picker properties.

--- cypress/e2e/other.cy.ts
+++ cypress/e2e/other.cy.ts
@@ -24,4 +24,34 @@ describe('Other Tests', () => {
 
     cy.get('@globalListener').should('have.been.calledOnce')
   })
+
+  it('should have the correct set of properties', () => {
+    const picker = datepicker(testElements.singleInput, {
+      selectedDate: new Date(),
+    })
+    const expectedPropertiesData = [
+      {propertyName: 'calendarContainer', type: '[object HTMLDivElement]'},
+      {propertyName: 'currentDate', type: '[object Date]'},
+      {propertyName: 'selectedDate', type: '[object Date]'},
+      {propertyName: 'remove', type: '[object Function]'},
+      {propertyName: 'navigate', type: '[object Function]'},
+      {propertyName: 'selectDate', type: '[object Function]'},
+      {propertyName: 'setMin', type: '[object Function]'},
+      {propertyName: 'setMax', type: '[object Function]'},
+      {propertyName: 'show', type: '[object Function]'},
+      {propertyName: 'hide', type: '[object Function]'},
+      {propertyName: 'toggleCalendar', type: '[object Function]'},
+      {propertyName: 'toggleOverlay', type: '[object Function]'},
+    ]
+    const expectedPropertyNames = expectedPropertiesData.map(
+      ({propertyName}) => propertyName
+    )
+
+    expect(Object.keys(picker)).to.have.members(expectedPropertyNames)
+
+    expectedPropertiesData.forEach(({propertyName, type}) => {
+      const property = picker[propertyName]
+      expect({}.toString.call(property)).to.equal(type)
+    })
+  })
 })

b8c45f7abbe6aaccb91c27cab3b49cc489e5d97b
Qodesmith <theqodesmith@gmail.com>
2023-02-18 20:53:19 -0500

    Fixed bug with checkbox and sliders.

--- sandbox/useSliderValuesCheckbox.tsx
+++ sandbox/useSliderValuesCheckbox.tsx
@@ -2,60 +2,58 @@ import {useRecoilValue} from 'recoil'
 import {DatepickerInstance, DaterangePickerInstance} from '../src/types'
 import {sliderAtomFamily} from './state'
 import {initialSliderValues} from './state'
-import {useCallback, useState} from 'react'
+import {useCallback, useLayoutEffect, useState} from 'react'
 
 export default function useSliderValuesCheckbox(
   picker: DatepickerInstance | DaterangePickerInstance | null,
   exemptId?: string
 ) {
   const [isChecked, setIsChecked] = useState(false)
-  const [isReset, setIsReset] = useState(false)
   const fontSize = useRecoilValue(sliderAtomFamily(initialSliderValues[0]))
   const dpSize = useRecoilValue(sliderAtomFamily(initialSliderValues[1]))
   const dpWidth = useRecoilValue(sliderAtomFamily(initialSliderValues[2]))
 
   const handleOnChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
-      const {checked} = e.target
-      setIsChecked(checked)
-      if (checked) {
-        setIsReset(false)
-      }
+      setIsChecked(e.target.checked)
     },
     []
   )
 
-  // TODO - this conditional logic is buggy. The `DatepickerAttachedToDiv` is
-  // initially showing even with `alwaysShow: false`.
-  if (isChecked) {
-    picker?.calendarContainer.parentElement?.style.setProperty(
-      'font-size',
-      `${fontSize}em`
-    )
-    picker?.calendarContainer.style.setProperty('--dp-size', `${dpSize}em`)
-    picker?.calendarContainer.style.setProperty('--dp-width', `${dpWidth}em`)
-  } else if (!isReset) {
-    picker?.calendarContainer.parentElement?.style.setProperty(
-      'font-size',
-      `${initialSliderValues[0].initialValue}em`
-    )
-    picker?.calendarContainer.style.setProperty(
-      '--dp-size',
-      `${initialSliderValues[1].initialValue}em`
-    )
-    picker?.calendarContainer.style.setProperty(
-      '--dp-width',
-      `${initialSliderValues[2].initialValue}em`
-    )
-    setIsReset(true)
-  }
+  useLayoutEffect(() => {
+    if (isChecked) {
+      picker?.calendarContainer.parentElement?.style.setProperty(
+        'font-size',
+        `${fontSize}em`
+      )
+      picker?.calendarContainer.style.setProperty('--dp-size', `${dpSize}em`)
+      picker?.calendarContainer.style.setProperty('--dp-width', `${dpWidth}em`)
+    }
+  }, [fontSize, dpSize, dpWidth, isChecked])
+
+  useLayoutEffect(() => {
+    if (!isChecked) {
+      console.log('resetting calendar container styles')
+      picker?.calendarContainer.parentElement?.style.setProperty(
+        'font-size',
+        `${initialSliderValues[0].initialValue}em`
+      )
+      picker?.calendarContainer.style.setProperty(
+        '--dp-size',
+        `${initialSliderValues[1].initialValue}em`
+      )
+      picker?.calendarContainer.style.setProperty(
+        '--dp-width',
+        `${initialSliderValues[2].initialValue}em`
+      )
+    }
+  }, [isChecked])
 
   const jsx = (
     <div className="checkbox-container">
       <span className="checkbox-text">check to apply slider styles</span>
       <input
         type="checkbox"
-        checked={isChecked}
         onChange={handleOnChange}
         data-exempt-id={exemptId}
       />

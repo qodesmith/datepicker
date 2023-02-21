d1c5f47289af681a784fc1ade805d8d7c720ed2f
Qodesmith <theqodesmith@gmail.com>
2023-02-21 08:29:40 -0500

    Updated DatepickerWhatever to be able to play with datepicker selectors.

--- sandbox/DatepickerWhatever.tsx
+++ sandbox/DatepickerWhatever.tsx
@@ -1,15 +1,25 @@
+import {useMemo} from 'react'
 import {useDatepicker} from './useDatepicker'
 
 export default function DatepickerWhatever() {
   const [jsx, picker] = useDatepicker({
     pickerKey: 'DatepickerWhatever',
-    type: 'div',
-    options: {},
+    type: 'input',
+    // selector: '.hello',
+    options: {alwaysShow: true},
   })
 
   return (
     <section>
       <h3>Datepicker Whatever</h3>
+
+      {/* Use this div to test string selectors with datepicker. */}
+      <div
+        className="hello"
+        id="world"
+        dangerouslySetInnerHTML={useMemo(() => ({__html: ''}), [])}
+      />
+
       {jsx}
     </section>
   )

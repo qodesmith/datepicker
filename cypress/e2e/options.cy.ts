45d1a3121b4fb01e6796af4555dfe4c35a10e33e
Qodesmith <theqodesmith@gmail.com>
2023-02-23 23:22:12 -0500

    Added tests for options - formatter.

---
+++ cypress/e2e/options.cy.ts
@@ -0,0 +1,35 @@
+import {Datepicker} from '../../src/types'
+import {days, testElementIds} from '../selectors'
+
+describe('Options', () => {
+  let datepicker: Datepicker
+
+  beforeEach(() => {
+    cy.visit(Cypress.env('TEST_DEV_LOCALHOST'))
+    cy.window().then(global => {
+      // @ts-ignore this will be available.
+      datepicker = global.datepicker
+    })
+  })
+
+  describe('formatter', () => {
+    it('should sanitize the input field value when selecting a date', () => {
+      const startDate = new Date(2023, 1)
+      const dayNum = 5
+      const picker = datepicker(testElementIds.singleInput, {
+        startDate,
+        alwaysShow: true,
+        formatter(date) {
+          return `Year: ${date.getFullYear()} Month: ${date.getMonth()} Day: ${date.getDate()}`
+        },
+      })
+
+      cy.get(testElementIds.singleInput).should('have.value', '')
+      cy.get(days.day).contains(dayNum).click()
+      cy.get(testElementIds.singleInput).should(
+        'have.value',
+        `Year: 2023 Month: 1 Day: ${dayNum}`
+      )
+    })
+  })
+})

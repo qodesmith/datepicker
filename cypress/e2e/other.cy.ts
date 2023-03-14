a1f815e20371dcf64044098cb3b93711bc9d9b23
Qodesmith <theqodesmith@gmail.com>
2023-03-14 09:52:54 -0400

    Added basic test for disabled date class.

--- cypress/e2e/other.cy.ts
+++ cypress/e2e/other.cy.ts
@@ -1,5 +1,5 @@
 import {Datepicker} from '../../src/types'
-import {testElementIds} from '../selectors'
+import {days, daysCls, testElementIds} from '../selectors'
 
 describe('Other Tests', () => {
   let datepicker: Datepicker
@@ -10,6 +10,37 @@ describe('Other Tests', () => {
       // @ts-ignore this will be available.
       datepicker = global.datepicker
     })
+  })
+
+  // TODO - refactor tests to only check for disabled class, not having to test for clicks.
+  it('should ignore clicks on days with the disabled date class', () => {
+    const startDate = new Date(2023, 2)
+    const picker = datepicker(testElementIds.singleInput, {
+      alwaysShow: true,
+      startDate,
+    })
+
+    // Arbitrarily add it to a single day.
+    cy.get(days.displayedDays)
+      .contains('6')
+      .should('not.have.class', daysCls.disabledDate)
+      .then($day => {
+        expect(picker.selectedDate).to.be.undefined
+        $day.addClass(daysCls.disabledDate)
+      })
+      .should('have.class', daysCls.disabledDate)
+      .click()
+      .then(() => {
+        expect(picker.selectedDate).to.be.undefined
+      })
+      .then($day => {
+        $day.removeClass(daysCls.disabledDate)
+      })
+      .should('not.have.class', daysCls.disabledDate)
+      .click()
+      .then(() => {
+        expect(picker.selectedDate).to.deep.equal(new Date(2023, 2, 6))
+      })
   })
 
   it('should only add an event listener on the document once', () => {

509afd1796c99eb3f4a7d247b6e1e1a0d1337d89
Qodesmith <theqodesmith@gmail.com>
2023-03-23 07:33:01 -0400

    Generating multiple versions + a types .d.ts file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
+import dts from 'vite-plugin-dts'
 import './commonEnv.js'
 
 export default defineConfig(({mode}) => {
   const isTest = mode === 'test'
   const envPortKey = isTest ? 'TEST_DEV_PORT' : 'DEV_PORT'
-  const plugins = isTest ? [] : [react()]
   const root = isTest ? './cypress/test-app' : './'
+  const plugins = (() => {
+    switch (mode) {
+      case 'production':
+        return [dts({rollupTypes: true})]
+      case 'test':
+        return []
+      default:
+        return [react()]
+    }
+  })()
 
   return {
     plugins,
@@ -18,9 +28,10 @@ export default defineConfig(({mode}) => {
     },
     build: {
       lib: {
+        formats: ['umd', 'cjs', 'es'],
         entry: resolve(__dirname, 'src/datepicker.ts'),
-        name: 'datepicker',
-        fileName: 'datepicker',
+        name: 'datepicker', // Exposed global variable.
+        fileName: 'datepicker', // Name of the generated file.
       },
     },
     clearScreen: !isTest,

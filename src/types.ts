7b6471ee7ce6f3f21cbb471822f6140c900e2abd
Qodesmith <theqodesmith@gmail.com>
2023-04-19 08:55:46 -0400

    SelectorData moved to a separate file.

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ import type {PickerElements} from './utilsCreateCalendar'
 import type {Expand, ExpandRecursively} from './types/expand'
 import {userEvents} from './constants'
 import {datepicker} from './datepicker'
+import {SelectorData} from './types/privatePicker'
 
 // TODO - ensure all types are being used. Remove export if not being consumed elsewhere.
 
@@ -649,54 +650,3 @@ export type DaterangePickerInstancePair = [
   DaterangePickerInstance,
   DaterangePickerInstance
 ]
-
-export type SelectorData = {
-  /**
-   * If the selector is an input, this will be the parent element or, in the
-   * case of a Shadow DOM, possibly the associated custom element.
-   *
-   * If the selector is not an element, then this will be the selector itself.
-   * I.e., `containingElement` and `el` will be the same element.
-   */
-  containingElement: HTMLElement
-
-  /**
-   * When Datepicker is associated with an input element, the calendar needs to
-   * be positioned relative to that input. In order to accomplish this, the
-   * input's parent element needs to be explicitly positioned.
-   *
-   * If the input's parent element doesn't contain any positioning already,
-   * inline styles of `position: relative` will be added to it. This function
-   * is a closure that captures all the necessary context in order to safely
-   * undo any styling datepicker has applied.
-   */
-  revertStyling?(): void
-} & (
-  | {
-      /**
-       * Indicates wether the associated selector represents an input element.
-       */
-      isInput: true
-
-      /**
-       * The element associated with the datepicker calendar. For any non-input
-       * elements, the calendar will become its child.
-       */
-      el: HTMLInputElement
-      revertStyling(): void
-    }
-  | {
-      /**
-       * Indicates wether the associated selector represents an input element.
-       */
-      isInput: false
-
-      /**
-       * The element associated with the datepicker calendar. For any non-input
-       * element, the calendar will become its child.
-       *
-       * For non-input elements, `el` and `containingElement` will be equal.
-       */
-      el: HTMLElement
-    }
-)
